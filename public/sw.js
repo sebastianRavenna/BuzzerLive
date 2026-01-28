5eac13bb604c098df1b26ebff775c6042a0de69e
Sebastian Ravenna <sebastian.ravenna@gmail.com>
2026-01-28 20:02:12 -0300

    logo_en_home

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buzzerlive-v20'; // Incrementar versión en cada deploy
+const CACHE_NAME = 'buzzerlive-v21'; // Incrementar versión en cada deploy
 const urlsToCache = [
   '/',
   '/index.html',
